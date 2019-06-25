3a2755e1fc67ecb4f25a5ee58198b96b5fb4c1eb
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-25 15:20:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ ReactDOM.render(
   container,
   document.getElementById('global')
 );
+
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', {className:'brown'}, 'Chocolate'),
+        React.createElement('li', {className:'white'}, 'Vanilla'),
+        React.createElement('li', {className:'yellow'}, 'Banana')
+      ]
+    )
+  );
+
+  
