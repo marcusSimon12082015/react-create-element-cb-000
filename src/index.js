f0530b31f2bbe88cadaeee34a288ba08eb2e4809
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-25 14:59:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const title = React.createElement('h1',{},'My First React code');
+ReactDOM.render(
+  title,
+  document.getElementById('global')
+);
